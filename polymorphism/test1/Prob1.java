7012c1a6715d04beca057a6fb18597f7b7940fa2
KeyM60 <86758277+KeyM60@users.noreply.github.com>
2024-07-01 22:33:45 +0900

    Update Prob1.java

--- polymorphism/test1/Prob1.java
+++ polymorphism/test1/Prob1.java
@@ -5,7 +5,9 @@ public class Prob1 {
   public static void main(String[] args) {
 
     Truck truck = new Truck("봉고3", 60, 4);
+    System.out.println("트럭 : 바퀴 " + truck.wheelNumber + " 개 입니다.");
     Bike bike = new Bike("메리다3", 15, 2);
+    System.out.println("자전거 : 바퀴 " + bike.wheelNumber + " 개 입니다.");
 
     truck.speedUp(2);
     bike.speedUp(2);
