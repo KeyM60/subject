ac8a6abaeafaaf3691218c2a1d5834cc1bbc0a11
KeyM60 <86758277+KeyM60@users.noreply.github.com>
2024-07-02 10:32:53 +0900

    Add files via upload

--- polymorphism/test4/VideoShop.java
+++ polymorphism/test4/VideoShop.java
@@ -1,17 +1,16 @@
-package subject.polymorphism.test4;
+
 
 public class VideoShop {
-
-  public static void main(String[] args) {
-    Content[] content = new Content[3];
-    content[0] = new Video("ì´í´ë¦½ìŠ¤", "new");
-    content[1] = new Video("ì²´í¬ì™•", "comic");
-    content[2] = new Video("ë½€ë¡±ë½€ë¡±ë½€ë¡œë¡œ2", "child");
-    for (int i = 0; i < content.length; i++) {
-      content[i].totalPrice();
-      content[i].show();
-    }
-
-  }
+	public static void main(String[] args) {
+		Content[] content = new Content[3];
+		content[0] = new Video("ÀÌÅ¬¸³½º","new");
+		content[1] = new Video("Ã¼Æ÷¿Õ","comic");
+		content[2] = new Video("»Ç·Õ»Ç·Õ»Ç·Î·Î2","child");
+		for (int i = 0; i < content.length; i++) {
+			content[i].totalPrice();
+			content[i].show();
+		}
+		
+	}
 
 }
