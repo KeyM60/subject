eae0a7c7261190ef47a454cd797dbde5965a3b4e
KeyM60 <86758277+KeyM60@users.noreply.github.com>
2024-07-04 09:29:45 +0900

    Update Search.java

--- Jdbc_member/CRUD/Search.java
+++ Jdbc_member/CRUD/Search.java
@@ -1,10 +1,10 @@
 package day16.jdbcEx.CRUD;
 
-import day16.jdbcEx.Connection.Jdbc_conn;
+import static day16.jdbcEx.Connection.Jdbc_conn.connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class Search extends Jdbc_conn {
+public class Search {
 
   public static void memberSearch() {
     String query = "select * from member";
